77e59b5c1d87cedd250acfb0f047bc7a190c6b60
newjava95 <410898362@qq.com>
2020-05-07 16:30:24 +0800

    Site updated: 2020-05-07 16:30:23

---
+++ js/src/post-details.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(document).on('click', '.fold_hider', function(){
+        $('>.fold', this.parentNode).slideToggle();
+        $('>:first', this).toggleClass('open');
+    });
+    
+    $("div.fold").css("display","open");
+});
